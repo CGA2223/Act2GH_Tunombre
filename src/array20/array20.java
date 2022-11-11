0f6b8acdcbec888a57425c4d0b58f84771e28666
CGA2223 <a_carlos.gil.albanil@iespablopicasso.es>
2022-11-11 13:47:01 +0100

    ignore

---
+++ src/array20/array20.java
@@ -0,0 +1,18 @@
+package array20;
+
+public class array20 {
+
+  public static void main(String[] args) {
+    
+      int[] numerosAleatoriosDelUnoAlCien;
+      numerosAleatoriosDelUnoAlCien = new int [20];
+      
+      for (int i = 0; i < 20; i++) {
+        numerosAleatoriosDelUnoAlCien[i] = (int) (Math.random() * 10 + 1);
+      }
+
+    
+
+  }
+
+}
